9f0170155aa8928912e6e46d2423c72aaa7353c2
longlv <longlevan296@gmail.com>
2024-07-22 16:34:32 +0700

    finish issues 2

--- app/src/am_longlv/com/example/asian/MainActivity.java
+++ app/src/am_longlv/com/example/asian/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.asian;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button exercise1, exercise2, exercise3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        InitUI();
+    }
+
+    private void InitUI() {
+        exercise1 = findViewById(R.id.btn_exercise1);
+        exercise2 = findViewById(R.id.btn_exercise2);
+        exercise3 = findViewById(R.id.btn_exercise3);
+
+        exercise1.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, Exercise1Activity.class));
+        });
+        exercise2.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, Exercise2Activity.class));
+        });
+        exercise3.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, Exercise3Activity.class));
+        });
     }
 }
