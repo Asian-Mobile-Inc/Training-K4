d2996b2654710e9aa60834dfbcb62a324ac6cf73
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-07-23 12:04:04 +0700

    doing issues3 activity

--- app/src/am_huynn/com/example/asian/MainActivity.java
+++ app/src/am_huynn/com/example/asian/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.asian;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Patterns;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.regex.Pattern;
 
@@ -16,5 +23,89 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+        EditText edt_email = findViewById(R.id.edtEmail);
+        EditText edtPassword = findViewById(R.id.edtPassword);
+
+        btnLogin.setEnabled(false);
+        validatorEmailAndPassword(edt_email, edtPassword,btnLogin);
+
+        btnLogin.setOnClickListener(view -> {
+            showSnackBar(view, "Đăng nhập thành công", 200);
+        });
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+
+    private void validatorEmailAndPassword(EditText edtEmail, EditText edtPassword, Button btn) {
+        final String PASSWORD_PATTERN = "^(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$";
+
+        edtEmail.addTextChangedListener(
+                new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                        if (!Patterns.EMAIL_ADDRESS.matcher(edtEmail.getText().toString()).matches()) {
+                            btn.setEnabled(false);
+                            edtEmail.setError("Email không đúng định dạng");
+                        }else {
+                            btn.setEnabled(true);
+                        }
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable editable) {
+
+                    }
+                }
+        );
+
+
+        edtPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                final String PASSWORD_TEXT = edtPassword.getText().toString();
+
+                if (PASSWORD_TEXT.length() < 8) {
+                    edtPassword.setError("Mật khẩu có nhiều hơn 8 ký tự");
+                    btn.setEnabled(false);
+                } else if (!Pattern.compile(PASSWORD_PATTERN).matcher(PASSWORD_TEXT).matches()) {
+                    edtPassword.setError("Mật khẩu có ít nhất 1 ký tự viết hoa và ký tự đặt biệt");
+                    btn.setEnabled(false);
+                }else {
+                    btn.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+
+    public void showSnackBar(View view, String message, int duration) {
+        Snackbar.make(view, message, duration).show();
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        View view = activity.findViewById(android.R.id.content);
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 }
--- app/src/am_huynn/com/example/asian/MainActivity3.java
+++ app/src/am_huynn/com/example/asian/MainActivity3.java
@@ -5,7 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -13,5 +17,74 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        EditText edtName = findViewById(R.id.edt_name);
+        EditText edtCard = findViewById(R.id.edt_cmnd);
+        EditText edtInformation = findViewById(R.id.edt_more_information);
+        Button btnSend = findViewById(R.id.btn_send);
+
+        validatorEmpty(edtName, btnSend);
+        validatorEmpty(edtCard, btnSend);
+        validatorMoreInformation(edtInformation,btnSend);
+
+        btnSend.setOnClickListener(view -> {
+            showSnackBar(view, "Gửi thành công", 200);
+        });
+    }
+
+    private void validatorEmpty(EditText editText, Button btn) {
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (editText.getText().toString().isEmpty()) {
+                    editText.setError("Không để trống giá trị này");
+                    btn.setEnabled(false);
+                } else {
+                    btn.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+
+    private void validatorMoreInformation(EditText edtInformation, Button btn) {
+        edtInformation.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (edtInformation.getText().toString().isEmpty()) {
+                    edtInformation.setError("Nhập thông tin bổ sung");
+                    btn.setEnabled(false);
+                } else if (edtInformation.getText().toString().length() < 100) {
+                    edtInformation.setError("Thông tin nhiều hơn 100 ký tự");
+                    btn.setEnabled(false);
+                }else {
+                    btn.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+
+    public void showSnackBar(View view, String message, int duration) {
+        Snackbar.make(view, message, duration).show();
     }
 }
+
+
