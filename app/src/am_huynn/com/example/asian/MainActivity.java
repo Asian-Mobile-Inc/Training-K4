515405b72b56b039d6fa6231bbc4ae99b06efccc
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-07-23 09:59:49 +0700

    update code UI & code issues2 validate EditText

--- app/src/am_huynn/com/example/asian/MainActivity.java
+++ app/src/am_huynn/com/example/asian/MainActivity.java
@@ -3,11 +3,76 @@ package com.example.asian;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button BTN_LOGIN = findViewById(R.id.btn_login);
+        final EditText EDT_EMAIL = findViewById(R.id.edt_email);
+        final EditText EDT_PASSWORD = findViewById(R.id.edt_password);
+
+        validatorEmail(EDT_EMAIL);
+        validatorPassword(EDT_PASSWORD);
+    }
+
+    private void validatorEmail(EditText edtEmail) {
+        edtEmail.addTextChangedListener(
+                new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                        if (!Patterns.EMAIL_ADDRESS.matcher(edtEmail.getText().toString()).matches()) {
+                            edtEmail.setError("Email không đúng định dạng");
+                        }
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable editable) {
+
+                    }
+                }
+        );
+    }
+
+    private void validatorPassword(EditText edtPassword) {
+
+        final String PASSWORD_PATTERN = "^(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$";
+
+        edtPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                final String PASSWORD_TEXT = edtPassword.getText().toString();
+
+                if (PASSWORD_TEXT.length() < 8) {
+                    edtPassword.setError("Mật khẩu có nhiều hơn 8 ký tự");
+                } else if (!Pattern.compile(PASSWORD_PATTERN).matcher(PASSWORD_TEXT).matches()) {
+                    edtPassword.setError("Mật khẩu có ít nhất 1 ký tự viết hoa và ký tự đặt biệt");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
--- app/src/am_huynn/com/example/asian/MainActivity3.java
+++ app/src/am_huynn/com/example/asian/MainActivity3.java
@@ -3,6 +3,9 @@ package com.example.asian;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -10,5 +13,52 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        validatorEmpty(findViewById(R.id.edt_name));
+        validatorEmpty(findViewById(R.id.edt_cmnd));
+        validatorMoreInformation(findViewById(R.id.edt_more_information));
+    }
+
+    private void validatorEmpty(EditText editText) {
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (editText.getText().toString().isEmpty()) {
+                    editText.setError("Không để trống giá trị này");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+
+    private void validatorMoreInformation(EditText edtInformation) {
+        edtInformation.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (edtInformation.getText().toString().isEmpty()) {
+                    edtInformation.setError("Nhập thông tin bổ sung");
+                } else if (edtInformation.getText().toString().length() < 100) {
+                    edtInformation.setError("Thông tin nhiều hơn 100 ký tự");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
