3fbd60467d0b15cb6bf3ca9ad161a7472e1db1f5
longlv <longlevan296@gmail.com>
2024-07-23 10:15:19 +0700

    save fixbug issues2

--- app/src/am_longlv/com/example/asian/MainActivity.java
+++ app/src/am_longlv/com/example/asian/MainActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.asian.fragment.Issues4_1Fragment;
 
-    Button exercise1, exercise2, exercise3;
+public class MainActivity extends AppCompatActivity implements Issues4_1Fragment.OnFragmentChangeListener {
+
+    Button exercise1, exercise2, exercise3,btn_issues4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         exercise1 = findViewById(R.id.btn_exercise1);
         exercise2 = findViewById(R.id.btn_exercise2);
         exercise3 = findViewById(R.id.btn_exercise3);
+        btn_issues4 = findViewById(R.id.btn_issues4);
 
         exercise1.setOnClickListener(v -> {
             startActivity(new Intent(MainActivity.this, Exercise1Activity.class));
@@ -30,5 +34,12 @@ public class MainActivity extends AppCompatActivity {
         exercise3.setOnClickListener(v -> {
             startActivity(new Intent(MainActivity.this, Exercise3Activity.class));
         });
+        btn_issues4.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, Issues4Activity.class));
+        });
+    }
+    @Override
+    public void onFragmentChange(String tag) {
+        Toast.makeText(this, "Fragment " + tag, Toast.LENGTH_SHORT).show();
     }
 }
