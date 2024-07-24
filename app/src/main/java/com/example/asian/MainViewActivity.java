46607dba9ced52fbecb944ec1a163872a131f4ca
Kzonw <sonhoang1432@gmail.com>
2024-07-24 10:06:30 +0700

    fix warning,eror report from ide, fix issues from mentor review

--- app/src/main/java/com/example/asian/MainViewActivity.java
+++ app/src/main/java/com/example/asian/MainViewActivity.java
@@ -9,6 +9,6 @@ public class MainViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_activity);
     }
 }
