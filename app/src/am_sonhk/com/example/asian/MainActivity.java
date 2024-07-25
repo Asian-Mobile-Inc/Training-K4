0cd9fa794f2c5e7ad15cd4b220280c3c78b5713f
Kzon <sonhoang1432@gmail.com>
2024-07-25 11:28:02 +0700

    remove ignore, fix style

--- app/src/am_sonhk/com/example/asian/MainActivity.java
+++ app/src/am_sonhk/com/example/asian/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.asian;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mBtnExerciseLogin, mBtnExerciseCal, mBtnExerciseUpdateInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity);
+        setContentView(R.layout.main_activity);
+        initUI();
+        initListener();
+    }
+
+    private void initUI() {
+        mBtnExerciseLogin = findViewById(R.id.btnExerciseLogin);
+        mBtnExerciseCal = findViewById(R.id.btnExerciseCal);
+        mBtnExerciseUpdateInfo = findViewById(R.id.btnExerciseUpdateInfo);
+    }
+
+    private void initListener() {
+        mBtnExerciseLogin.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, LoginActivity.class)));
+        mBtnExerciseCal.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, CalculateActivity.class)));
+        mBtnExerciseUpdateInfo.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, UpdateInfoActivity.class)));
     }
 }
