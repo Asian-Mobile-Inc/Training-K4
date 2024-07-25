a0c2eb356539d9c5f734309c79395d60be4f33a2
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-07-25 16:49:36 +0700

    send data from login

--- app/src/am_huynn/com/example/asian/ui/DataActivity.java
+++ app/src/am_huynn/com/example/asian/ui/DataActivity.java
@@ -1,16 +1,33 @@
 package com.example.asian.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.asian.R;
 
 public class DataActivity extends AppCompatActivity {
+    private TextView mTvEmail, mTvPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
+        initView();
+        getData();
+    }
+
+    private void getData() {
+        String email = getString(R.string.email) + getIntent().getStringExtra("keyEmail");
+        String password = getString(R.string.password) + getIntent().getStringExtra("keyPassword");
+
+        mTvEmail.setText(email);
+        mTvPassword.setText(password);
+    }
+
+    private void initView() {
+        mTvEmail = findViewById(R.id.tvEmail);
+        mTvPassword = findViewById(R.id.tvPassword);
     }
 }
--- app/src/am_huynn/com/example/asian/ui/FacebookActivity.java
+++ app/src/am_huynn/com/example/asian/ui/FacebookActivity.java
@@ -1,15 +1,88 @@
 package com.example.asian.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.asian.R;
 
 public class FacebookActivity extends AppCompatActivity {
+    private EditText mEdtEmail, mEdtPassword;
+    private Button mBtnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facebook);
+        initView();
+        initListener();
+    }
+
+    private void initView() {
+        mEdtEmail = findViewById(R.id.edtEmail);
+        mEdtPassword = findViewById(R.id.edtPassword);
+        mBtnLogin = findViewById(R.id.btnLogin);
+    }
+
+    private void initListener() {
+        mBtnLogin.setOnClickListener(view -> {
+            if (!validateEmail() || !validatePassword()) {
+                return;
+            }
+
+            String email = mEdtEmail.getText().toString();
+            String password = mEdtPassword.getText().toString();
+
+            Intent intent = new Intent(this, DataActivity.class);
+            intent.putExtra("keyEmail", email);
+            intent.putExtra("keyPassword", password);
+            startActivity(intent);
+        });
+    }
+
+    private boolean validateEmail() {
+        String value = mEdtEmail.getText().toString();
+
+        if (value.isEmpty()) {
+            mEdtEmail.setError(getString(R.string.field_can_not_empty));
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(value).matches()) {
+            mEdtEmail.setError(getString(R.string.invalid_email));
+            return false;
+        } else {
+            mEdtEmail.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validatePassword() {
+        final String REGEX_INCLUDE_NUMBER = "^.*\\d.*";
+        final String REGEX_NORMAL_CHARACTER = "^.*[a-z].*";
+        final String REGEX_SPECIAL_CHARACTER = "^.*[@#$%^&+=].*";
+        String value = mEdtPassword.getText().toString();
+
+        if (value.isEmpty()) {
+            mEdtPassword.setError(getString(R.string.field_can_not_empty));
+            return false;
+        } else if (value.length() < 8) {
+            mEdtPassword.setError(getString(R.string.password_more_than_eight_characters));
+            return false;
+        } else if (!value.matches(REGEX_INCLUDE_NUMBER)) {
+            mEdtPassword.setError(getString(R.string.password_must_include_number));
+            return false;
+        } else if (!value.matches(REGEX_NORMAL_CHARACTER)) {
+            mEdtPassword.setError(getString(R.string.password_must_include_normal_characters));
+            return false;
+        } else if (!value.matches(REGEX_SPECIAL_CHARACTER)) {
+            mEdtPassword.setError(getString(R.string.password_must_include_special_characters));
+            return false;
+        } else {
+            mEdtPassword.setError(null);
+            return true;
+        }
     }
 }
