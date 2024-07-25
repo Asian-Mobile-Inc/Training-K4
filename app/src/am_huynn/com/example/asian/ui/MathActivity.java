a9c5dfd38f1bfb81d1f0afd5285ab95e50e6482f
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-07-25 16:51:56 +0700

    create calculator

--- app/src/am_huynn/com/example/asian/ui/MathActivity.java
+++ app/src/am_huynn/com/example/asian/ui/MathActivity.java
@@ -1,16 +1,107 @@
 package com.example.asian.ui;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.asian.R;
 
 public class MathActivity extends AppCompatActivity {
+    private TextView mTvResult;
+    private EditText mEdtNumberOne, mEdtNumberTwo;
+    private Button mBtnAddition, mBtnSubtraction, mBtnMultiplication, mBtnDivision;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_math);
+        initView();
+        initListener();
+    }
+
+    private void initView() {
+        mTvResult = findViewById(R.id.tvResult);
+        mEdtNumberOne = findViewById(R.id.edtNumberOne);
+        mEdtNumberTwo = findViewById(R.id.edtNumberTwo);
+        mBtnAddition = findViewById(R.id.btnAddition);
+        mBtnSubtraction = findViewById(R.id.btnSubtraction);
+        mBtnMultiplication = findViewById(R.id.btnMultiplication);
+        mBtnDivision = findViewById(R.id.btnDivision);
+    }
+
+    private void initListener() {
+        mBtnAddition.setOnClickListener(view -> {
+            String textNumberOne = mEdtNumberOne.getText().toString();
+            String textNumberTwo = mEdtNumberTwo.getText().toString();
+            if (isNumber(textNumberOne) && isNumber(textNumberTwo)) {
+                double numberOne = Double.parseDouble(textNumberOne);
+                double numberTwo = Double.parseDouble(textNumberTwo);
+                String result = getString(R.string.result) + (numberOne + numberTwo);
+                mTvResult.setText(result);
+            } else {
+                mTvResult.setText(getString(R.string.please_input_number));
+            }
+        });
+
+        mBtnSubtraction.setOnClickListener(view -> {
+            String textNumberOne = mEdtNumberOne.getText().toString();
+            String textNumberTwo = mEdtNumberTwo.getText().toString();
+            if (isNumber(textNumberOne) && isNumber(textNumberTwo)) {
+                double numberOne = Double.parseDouble(textNumberOne);
+                double numberTwo = Double.parseDouble(textNumberTwo);
+                String result = getString(R.string.result) + (numberOne - numberTwo);
+                mTvResult.setText(result);
+            } else {
+                mTvResult.setText(getString(R.string.please_input_number));
+            }
+        });
+
+        mBtnMultiplication.setOnClickListener(view -> {
+            String textNumberOne = mEdtNumberOne.getText().toString();
+            String textNumberTwo = mEdtNumberTwo.getText().toString();
+            if (isNumber(textNumberOne) && isNumber(textNumberTwo)) {
+                double numberOne = Double.parseDouble(textNumberOne);
+                double numberTwo = Double.parseDouble(textNumberTwo);
+                String result = getString(R.string.result) + (numberOne * numberTwo);
+                mTvResult.setText(result);
+            } else {
+                mTvResult.setText(getString(R.string.please_input_number));
+            }
+        });
+
+
+        mBtnDivision.setOnClickListener(view -> {
+            String textNumberOne = mEdtNumberOne.getText().toString();
+            String textNumberTwo = mEdtNumberTwo.getText().toString();
+            if (isNumber(textNumberOne) && isNumberOtherZero(textNumberTwo)) {
+                double numberOne = Double.parseDouble(textNumberOne);
+                double numberTwo = Double.parseDouble(textNumberTwo);
+                String result = getString(R.string.result) + (numberOne / numberTwo);
+                mTvResult.setText(result);
+            } else {
+                mTvResult.setText(getString(R.string.please_input_number_other_zero));
+            }
+        });
+    }
+
+    private boolean isNumber(String value) {
+        try {
+            Double.parseDouble(value);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private boolean isNumberOtherZero(String value) {
+        try {
+            double num = Double.parseDouble(value);
+            return num != 0;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
