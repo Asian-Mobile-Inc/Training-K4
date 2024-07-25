61e1a32df2e19fe830062eb5d7b7ed7b724acfd8
Kzon <sonhoang1432@gmail.com>
2024-07-25 08:18:57 +0700

    edit recycle view

--- app/src/am_sonhk/com/example/asian/MainActivity.java
+++ app/src/am_sonhk/com/example/asian/MainActivity.java
@@ -13,23 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initUI();
-        initListener();
-    }
-    private void initUI() {
-        mBtnTab1 = findViewById(R.id.btnTab1);
-        mBtnTab2 = findViewById(R.id.btnTab2);
-        mBtnTab3 = findViewById(R.id.btnTab3);
-    }
-    private void initListener() {
-        mBtnTab1.setOnClickListener(v -> {
-            startActivity(new Intent(MainActivity.this, TabOneActivity.class));
-        });
-        mBtnTab2.setOnClickListener(v -> {
-            startActivity(new Intent(MainActivity.this, TabTwoActivity.class));
-        });
-        mBtnTab3.setOnClickListener(v -> {
-            startActivity(new Intent(MainActivity.this, TabThreeActivity.class));
-        });
+
     }
 }
