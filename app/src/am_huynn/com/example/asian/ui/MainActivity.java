a1c517ebeecc12cf16e5f8cd23d19b1508005e26
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-07-25 11:03:05 +0700

    add MainActivity move three activity

---
+++ app/src/am_huynn/com/example/asian/ui/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.asian.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.asian.R;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mBtnToFacebook, mBtnToMath, mBtnToInformation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mBtnToFacebook = findViewById(R.id.btnFacebook);
+        mBtnToMath = findViewById(R.id.btnMath);
+        mBtnToInformation = findViewById(R.id.btnInformation);
+        setOnClickButton();
+    }
+
+    private void setOnClickButton() {
+        mBtnToFacebook.setOnClickListener(view -> {
+            Intent intent = new Intent(this, FacebookActivity.class);
+            startActivity(intent);
+        });
+
+        mBtnToMath.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MathActivity.class);
+            startActivity(intent);
+        });
+
+        mBtnToInformation.setOnClickListener(view -> {
+            Intent intent = new Intent(this, InformationActivity.class);
+            startActivity(intent);
+        });
+    }
+}
