39f8f2daa91bd3b63d3e7d24d6227cb523616230
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-07-25 14:34:01 +0700

    create method initView & initListener

--- app/src/am_huynn/com/example/asian/MainActivity.java
+++ app/src/am_huynn/com/example/asian/MainActivity.java
@@ -17,13 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initListener();
+    }
+
+    private void initView() {
         mBtnToFacebook = findViewById(R.id.btnFacebook);
         mBtnToMath = findViewById(R.id.btnMath);
         mBtnToInformation = findViewById(R.id.btnInformation);
-        setOnClickButton();
     }
 
-    private void setOnClickButton() {
+    private void initListener() {
         mBtnToFacebook.setOnClickListener(view -> {
             Intent intent = new Intent(this, FacebookActivity.class);
             startActivity(intent);
