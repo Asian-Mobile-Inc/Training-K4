c279a5626403010edbfba78c47ce74e0b5a9d8e4
Kzon <sonhoang1432@gmail.com>
2024-07-29 11:29:31 +0700

    fix string - breakline

--- app/src/am_sonhk/com/example/asian/UpdateInfoActivity.java
+++ app/src/am_sonhk/com/example/asian/UpdateInfoActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UpdateInfoActivity extends AppCompatActivity {
