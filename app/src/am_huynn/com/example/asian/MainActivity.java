6f725cb64654b8c88b7b079b7f599fe147c54c99
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-07-30 16:16:01 +0700

    update main & file android manifest

--- app/src/am_huynn/com/example/asian/MainActivity.java
+++ app/src/am_huynn/com/example/asian/MainActivity.java
@@ -6,12 +6,13 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.asian.ui.DrawerNavigationActivity;
 import com.example.asian.ui.FacebookActivity;
 import com.example.asian.ui.InformationActivity;
 import com.example.asian.ui.MathActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private Button mBtnToFacebook, mBtnToMath, mBtnToInformation;
+    private Button mBtnToFacebook, mBtnToMath, mBtnToInformation, mBtnDrawerNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         mBtnToFacebook = findViewById(R.id.btnFacebook);
         mBtnToMath = findViewById(R.id.btnMath);
         mBtnToInformation = findViewById(R.id.btnInformation);
+        mBtnDrawerNavigation = findViewById(R.id.btnDrawerNavigation);
     }
 
     private void initListener() {
@@ -40,6 +42,11 @@ public class MainActivity extends AppCompatActivity {
 
         mBtnToInformation.setOnClickListener(view -> {
             Intent intent = new Intent(this, InformationActivity.class);
+            startActivity(intent);
+        });
+
+        mBtnDrawerNavigation.setOnClickListener(view -> {
+            Intent intent = new Intent(this, DrawerNavigationActivity.class);
             startActivity(intent);
         });
     }
