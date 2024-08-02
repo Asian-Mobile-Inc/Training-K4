f4b8aa12b36c29d4eb87163abc131fef92fac87d
longlv <longlevan296@gmail.com>
2024-08-02 09:06:33 +0700

    update getString bundle

--- app/src/am_longlv/com/example/asian/issuethree/IssueThreeActivity.java
+++ app/src/am_longlv/com/example/asian/issuethree/IssueThreeActivity.java
@@ -45,16 +45,16 @@ public class IssueThreeActivity extends AppCompatActivity {
         Bundle bundleFromLogin = getIntent().getBundleExtra(Constant.KEY_FROM_LOGIN);
         Bundle bundleFromUpdateInfo = getIntent().getBundleExtra(Constant.KEY_FROM_UPDATE_INFO);
         if (bundleFromLogin != null) {
-            mTvEmail.setText(getString(R.string.email_str_param, bundleFromLogin.get(Constant.KEY_EMAIL)));
-            mTvPassword.setText(getString(R.string.password_str_param, bundleFromLogin.get(Constant.KEY_PASSWORD)));
+            mTvEmail.setText(getString(R.string.email_str_param, bundleFromLogin.getString(Constant.KEY_EMAIL)));
+            mTvPassword.setText(getString(R.string.password_str_param, bundleFromLogin.getString(Constant.KEY_PASSWORD)));
             mLlDataFromLogin.setVisibility(View.VISIBLE);
             mLlDataFromUpdateInfo.setVisibility(View.GONE);
         } else if (bundleFromUpdateInfo != null) {
-            mTvName.setText(getString(R.string.name_str_param, bundleFromUpdateInfo.get(Constant.KEY_NAME)));
-            mTvIdCard.setText(getString(R.string.id_card_str_param, bundleFromUpdateInfo.get(Constant.KEY_ID_CARD)));
-            mTvDegree.setText(getString(R.string.degree_str_param, bundleFromUpdateInfo.get(Constant.KEY_DEGREE)));
-            mTvInterest.setText(getString(R.string.interest_str_param, bundleFromUpdateInfo.get(Constant.KEY_INTEREST)));
-            mTvMoreInfo.setText((getString(R.string.more_info_str_param, bundleFromUpdateInfo.get(Constant.KEY_MORE_INFO))));
+            mTvName.setText(getString(R.string.name_str_param, bundleFromUpdateInfo.getString(Constant.KEY_NAME)));
+            mTvIdCard.setText(getString(R.string.id_card_str_param, bundleFromUpdateInfo.getString(Constant.KEY_ID_CARD)));
+            mTvDegree.setText(getString(R.string.degree_str_param, bundleFromUpdateInfo.getString(Constant.KEY_DEGREE)));
+            mTvInterest.setText(getString(R.string.interest_str_param, bundleFromUpdateInfo.getString(Constant.KEY_INTEREST)));
+            mTvMoreInfo.setText((getString(R.string.more_info_str_param, bundleFromUpdateInfo.getString(Constant.KEY_MORE_INFO))));
             mLlDataFromLogin.setVisibility(View.GONE);
             mLlDataFromUpdateInfo.setVisibility(View.VISIBLE);
         }
