96d1918d33133b17b86b15e32aaca6159eb9ab2b
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-08-02 10:19:31 +0700

    create method CRUD database

--- app/src/am_huynn/com/example/asian/ex_sqlite/helper/UserSQLiteHelper.java
+++ app/src/am_huynn/com/example/asian/ex_sqlite/helper/UserSQLiteHelper.java
@@ -2,10 +2,14 @@ package com.example.asian.ex_sqlite.helper;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.example.asian.ex_sqlite.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserSQLiteHelper extends SQLiteOpenHelper {
     public static final String TABLE_USER = "user";
@@ -15,6 +19,7 @@ public class UserSQLiteHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "user.db";
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_CREATE = "create table " + TABLE_USER + "( " + COLUMN_USER_ID + " integer primary key autoincrement, " + COLUMN_USER_NAME + " text not null, " + COLUMN_AGE + " integer not null);";
+    private static final String[] ALL_COLUMN = {COLUMN_USER_ID, COLUMN_USER_NAME, COLUMN_AGE};
 
     public UserSQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -29,5 +34,37 @@ public class UserSQLiteHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_USER);
         onCreate(sqLiteDatabase);
+    }
+
+    public void addUser(User user) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_USER_NAME, user.getUserName());
+        values.put(COLUMN_AGE, user.getAge());
+        db.insert(TABLE_USER, null, values);
+        db.close();
+    }
+
+    public List<User> getAllUsers() {
+        List<User> listUser = new ArrayList<>();
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.query(TABLE_USER, ALL_COLUMN, null, null, null, null, null);
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            User user = new User(cursor.getInt(0), cursor.getString(1), cursor.getInt(2));
+            listUser.add(user);
+            cursor.moveToNext();
+        }
+        return listUser;
+    }
+
+    public void deleteUser(int id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_USER, COLUMN_USER_ID + " = " + id, null);
+    }
+
+    public void deleteAllUsers() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DELETE FROM " + TABLE_USER);
     }
 }
