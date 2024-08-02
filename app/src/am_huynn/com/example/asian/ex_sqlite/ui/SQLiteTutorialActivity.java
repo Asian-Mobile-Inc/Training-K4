1aa99e1105c574fd46a4c37ee5f30bb2c518a5f2
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-08-02 11:21:04 +0700

    update string & color

---
+++ app/src/am_huynn/com/example/asian/ex_sqlite/ui/SQLiteTutorialActivity.java
@@ -0,0 +1,14 @@
+package com.example.asian.ex_sqlite.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SQLiteTutorialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sqlite_tutorial);
+    }
+}
