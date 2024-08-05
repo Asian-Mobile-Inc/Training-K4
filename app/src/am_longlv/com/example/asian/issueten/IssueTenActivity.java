c5d93d0300590410c3957a9f1dfc34d37a806c1d
longlv <longlevan296@gmail.com>
2024-08-05 15:20:34 +0700

    update scale

--- app/src/am_longlv/com/example/asian/issueten/IssueTenActivity.java
+++ app/src/am_longlv/com/example/asian/issueten/IssueTenActivity.java
@@ -17,8 +17,8 @@ public class IssueTenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_issue_ten);
-//        initUI();
-//        initData();
+        initUI();
+        initData();
     }
 
     private void initUI() {
