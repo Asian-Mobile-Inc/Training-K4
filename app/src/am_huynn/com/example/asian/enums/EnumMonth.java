ecb268393b720c55c3c5d0a6f65e6fba69cee101
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-08-08 13:47:18 +0700

    create Model SellExpense

--- app/src/am_huynn/com/example/asian/enums/EnumMonth.java
+++ app/src/am_huynn/com/example/asian/enums/EnumMonth.java
@@ -1,2 +1,5 @@
-package com.example.asian.enums;public class EnumMonth {
+package com.example.asian.enums;
+
+public enum EnumMonth {
+    Jan, Fed, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec
 }
--- app/src/am_huynn/com/example/asian/model/SellExpense.java
+++ app/src/am_huynn/com/example/asian/model/SellExpense.java
@@ -1,2 +1,39 @@
-package com.example.asian.model;public class SellExpense {
+package com.example.asian.model;
+
+import com.example.asian.enums.EnumMonth;
+
+public class SellExpense {
+    private EnumMonth month;
+    private float sales;
+    private float expenses;
+
+    public SellExpense(EnumMonth month, int sales, int expenses) {
+        this.month = month;
+        this.sales = sales;
+        this.expenses = expenses;
+    }
+
+    public EnumMonth getMonth() {
+        return month;
+    }
+
+    public void setMonth(EnumMonth month) {
+        this.month = month;
+    }
+
+    public float getSales() {
+        return sales;
+    }
+
+    public void setSales(float sales) {
+        this.sales = sales;
+    }
+
+    public float getExpenses() {
+        return expenses;
+    }
+
+    public void setExpenses(float expenses) {
+        this.expenses = expenses;
+    }
 }
