51c4f4816813f7d57c4251e178e8d9159829a235
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-08-09 16:02:20 +0700

    create scale examples

--- app/src/am_huynn/com/example/asian/view/MyChartView.java
+++ app/src/am_huynn/com/example/asian/view/MyChartView.java
@@ -6,6 +6,8 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 
 import androidx.annotation.NonNull;
@@ -22,23 +24,30 @@ public class MyChartView extends View {
     private ArrayList<SellExpense> mSellExpenses;
     private Paint mPaintBlack;
     private TextPaint mTextPaint;
+    private TextPaint mTextCenterPaint;
     private Paint mPaintSales;
     private Paint mPaintExpenses;
     private ArrayList<Integer> mValues = new ArrayList<>();
 
+    private float mScaleFactor = 1.0f;
+
+    private ScaleGestureDetector mScaleGestureDetector;
+
     public MyChartView(Context context) {
         super(context);
+        initPaint();
     }
 
     public MyChartView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         initAttrs(context, attrs);
         initPaint();
+        mScaleGestureDetector = new ScaleGestureDetector(this.getContext(), new ScaleListener());
     }
 
     public void setSellExpenses(ArrayList<SellExpense> sellExpenses) {
         this.mSellExpenses = sellExpenses;
-        initPaint();
+        initValues();
     }
 
     public void setColorSale(int colorSale) {
@@ -58,6 +67,7 @@ public class MyChartView extends View {
     private void initPaint() {
         mPaintBlack = new Paint(Paint.ANTI_ALIAS_FLAG);
         mTextPaint = new TextPaint();
+        mTextCenterPaint = new TextPaint();
         mPaintSales = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaintExpenses = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaintBlack.setColor(getResources().getColor(R.color.black));
@@ -65,7 +75,10 @@ public class MyChartView extends View {
         mPaintBlack.setStrokeWidth(3);
         mTextPaint.setColor(getResources().getColor(R.color.black));
         mTextPaint.setTextAlign(Paint.Align.RIGHT);
-        mTextPaint.setTextSize(28);
+        mTextPaint.setTextSize(23);
+        mTextCenterPaint.setColor(getResources().getColor(R.color.black));
+        mTextCenterPaint.setTextAlign(Paint.Align.CENTER);
+        mTextCenterPaint.setTextSize(28);
         mPaintSales.setColor(mColorSale);
         mPaintSales.setStyle(Paint.Style.FILL);
         mPaintExpenses.setColor(mColorExpenses);
@@ -83,12 +96,10 @@ public class MyChartView extends View {
         if (mSellExpenses == null) {
             mSellExpenses = new ArrayList<>();
         }
-        initValues();
         drawChartFrame(canvas);
     }
 
     private void initValues() {
-
         float valueMax = 0;
         for (int i = 0; i < mSellExpenses.size(); i++) {
             if (mSellExpenses.get(i).getSales() > valueMax) {
@@ -108,29 +119,46 @@ public class MyChartView extends View {
     }
 
     private void drawChartFrame(Canvas canvas) {
-        int xStart = 200;
-        int yStart = 100;
-        int spaceItemMonth = 180;
-        int spaceItemValue = (getHeight() - yStart) / mValues.size(); // 10value = 40dp
+        int xStart = 13 * getWidth() / 100;
+        int yStart = 5 * getHeight() / 100;
+        int spaceItemMonth = (getWidth() - xStart) / 6;
+        int spaceItemValue = (getHeight() - (12 * getHeight() / 100)) / (mValues.size() - 1);
         int yEnd = (mValues.size() - 1) * spaceItemValue + yStart;
         int xEnd = mSellExpenses.size() * spaceItemMonth + xStart;
 
-        int spaceColumnChart = 60;
-        int spaceCenterColumn = 30;
+        int spaceColumnChart = spaceItemMonth / 3;
+        int spaceCenterColumn = spaceItemMonth / 6;
 
         canvas.drawLine(xStart, yStart, xStart, yEnd, mPaintBlack);
         canvas.drawLine(xStart, yEnd, xEnd, yEnd, mPaintBlack);
         for (int i = 0; i < mValues.size(); i++) {
             canvas.drawLine(xStart, i * spaceItemValue + yStart, xEnd, i * spaceItemValue + yStart, mPaintBlack);
-            canvas.drawText(mValues.get(mValues.size() - 1 - i) + "$", 190, i * spaceItemValue + yStart + 10, mTextPaint);
+            canvas.drawText(mValues.get(mValues.size() - 1 - i) + "$", xStart - 5, i * spaceItemValue + yStart + 10, mTextPaint);
         }
 
         for (int i = 0; i < mSellExpenses.size(); i++) {
-            mTextPaint.setTextAlign(Paint.Align.CENTER);
-            canvas.drawText(mSellExpenses.get(i).getMonth().name(), xStart + i * spaceItemMonth + spaceItemMonth / 2, yEnd + 40, mTextPaint);
+            canvas.drawText(mSellExpenses.get(i).getMonth().name(), xStart + i * spaceItemMonth + spaceItemMonth / 2, yEnd + 30, mTextCenterPaint);
 
-            canvas.drawRect(xStart + i * spaceItemMonth + spaceCenterColumn, yEnd - (mSellExpenses.get(i).getSales() / 10000 * (spaceItemValue / 2)), xStart + i * spaceItemMonth + spaceColumnChart + spaceCenterColumn, yEnd, mPaintSales);
-            canvas.drawRect(xStart + i * spaceItemMonth + spaceColumnChart + spaceCenterColumn, yEnd - (mSellExpenses.get(i).getExpenses() / 10000 * 40), xStart + i * spaceItemMonth + spaceColumnChart + spaceColumnChart + spaceCenterColumn, yEnd, mPaintExpenses);
+            canvas.drawRect(xStart + i * spaceItemMonth + spaceCenterColumn, yEnd - (mSellExpenses.get(i).getSales() / 20000 * spaceItemValue), xStart + i * spaceItemMonth + spaceColumnChart + spaceCenterColumn, yEnd, mPaintSales);
+            canvas.drawRect(xStart + i * spaceItemMonth + spaceColumnChart + spaceCenterColumn, yEnd - (mSellExpenses.get(i).getExpenses() / 20000 * spaceItemValue), xStart + i * spaceItemMonth + spaceColumnChart + spaceColumnChart + spaceCenterColumn, yEnd, mPaintExpenses);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mScaleGestureDetector.onTouchEvent(event);
+        return true;
+    }
+
+    class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(@NonNull ScaleGestureDetector detector) {
+            mScaleFactor = detector.getScaleFactor() - 1;
+            mScaleFactor += detector.getScaleFactor();
+//            mScaleFactor = Math.max(0.5f, Math.min(mScaleFactor, 2.5f));
+            setScaleX(mScaleFactor);
+            setScaleY(mScaleFactor);
+            return true;
         }
     }
 }
