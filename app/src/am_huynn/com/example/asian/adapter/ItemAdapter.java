55f3d7ff9aee810b50bee99b1c81df19729a7607
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-08-12 10:40:44 +0700

    create recyclerview $ class ItemDiffUtilCallBack

---
+++ app/src/am_huynn/com/example/asian/adapter/ItemAdapter.java
@@ -0,0 +1,98 @@
+package com.example.asian.adapter;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.asian.R;
+import com.example.asian.diff.ItemDiffUtilCallBack;
+import com.example.asian.model.Item;
+
+import java.util.ArrayList;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.MyViewHolder> {
+    private final ArrayList<Item> mItems;
+    private final Context mContext;
+    private IDeleteItem iDeleteItem;
+
+    public ItemAdapter(Context context, ArrayList<Item> items) {
+        this.mContext = context;
+        this.mItems = new ArrayList<>(items);
+    }
+
+    public void setData(ArrayList<Item> newItems) {
+        DiffUtil.DiffResult diffResult = DiffUtil.calculateDiff(new ItemDiffUtilCallBack(mItems, newItems));
+        mItems.clear();
+        this.mItems.addAll(newItems);
+        diffResult.dispatchUpdatesTo(this);
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(mContext).inflate(R.layout.item_view, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Item item = mItems.get(position);
+        holder.mTvItemName.setText(item.getName());
+
+//        holder.mLlItem.setOnClickListener(view -> {
+//            Dialog dialog = new Dialog(mContext);
+//            dialog.setContentView(R.layout.custom_dialog);
+//            dialog.show();
+//            TextView tvEditItem = dialog.findViewById(R.id.tvEditItem);
+//            TextView tvDeleteItem = dialog.findViewById(R.id.tvDeleteItem);
+//            tvDeleteItem.setOnClickListener(view1 -> {
+//                if (mContext instanceof IDeleteItem) {
+//                    iDeleteItem = (IDeleteItem) mContext;
+//                    iDeleteItem.deleteItem(item.getId());
+//                }
+//                dialog.dismiss();
+//            });
+//            tvEditItem.setOnClickListener(view1 -> {
+//                dialog.dismiss();
+//                Intent intent = new Intent(mContext, EditItemActivity.class);
+//                intent.putExtra("keyNameItem", item.getName());
+//                intent.putExtra("keyIdItem", item.getId());
+//                ((Activity) mContext).startActivityForResult(intent, 10000);
+//            });
+//        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+        private TextView mTvItemName;
+        private LinearLayout mLlItem;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            initView(itemView);
+        }
+
+        private void initView(View view) {
+            mTvItemName = view.findViewById(R.id.tvItemName);
+            mLlItem = view.findViewById(R.id.llItem);
+        }
+    }
+
+    public interface IDeleteItem {
+        void deleteItem(int id);
+    }
+}
--- app/src/am_huynn/com/example/asian/fragment/ViewFragment.java
+++ app/src/am_huynn/com/example/asian/fragment/ViewFragment.java
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.asian.R;
+import com.example.asian.adapter.ItemAdapter;
 import com.example.asian.model.Item;
 
 import java.util.ArrayList;
@@ -17,6 +18,8 @@ import java.util.ArrayList;
 public class ViewFragment extends Fragment {
     private final ArrayList<Item> mItems;
     private RecyclerView mRvItemsList;
+    private ItemAdapter mItemAdapter;
+
 
     public ViewFragment(ArrayList<Item> items) {
         this.mItems = new ArrayList<>(items);
@@ -26,10 +29,17 @@ public class ViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_view, container, false);
         initView(view);
+        setAdapter();
         return view;
     }
 
     private void initView(View view) {
         mRvItemsList = view.findViewById(R.id.rvItemsList);
+    }
+
+    private void setAdapter() {
+        mItemAdapter = new ItemAdapter(getContext(), mItems);
+        mRvItemsList.setAdapter(mItemAdapter);
+        mRvItemsList.setLayoutManager(new LinearLayoutManager(getActivity()));
     }
 }
