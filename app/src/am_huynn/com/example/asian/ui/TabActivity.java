60763f50a0bfba5e1513a1aa2199d25bed94c1a8
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-08-12 10:16:59 +0700

    create Ui TabActivity

---
+++ app/src/am_huynn/com/example/asian/ui/TabActivity.java
@@ -0,0 +1,16 @@
+package com.example.asian.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.asian.R;
+
+public class TabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab);
+    }
+}
