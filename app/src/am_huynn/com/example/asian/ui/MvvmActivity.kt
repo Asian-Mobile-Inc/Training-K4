0fdde3ab80a293580301cdc3ad9c8d6e68781115
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-08-13 14:19:37 +0700

    create MvvpActivity

---
+++ app/src/am_huynn/com/example/asian/ui/MvvmActivity.kt
@@ -0,0 +1,12 @@
+package com.example.asian.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.asian.R
+
+class MvvmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mvvm)
+    }
+}
