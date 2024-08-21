7383f4cdc8489279a98cde650e612dc7bfc0c2ca
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-08-21 08:52:58 +0700

    create pager adpter

--- app/src/am_huynn/com/example/asian/adapter/PagerAdapter.kt
+++ app/src/am_huynn/com/example/asian/adapter/PagerAdapter.kt
@@ -1,4 +1,19 @@
 package com.example.asian.adapter
 
-class PagerAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.asian.fragment.UsersFragment
+
+class PagerAdapter(fragmentActivity: FragmentActivity) :
+    FragmentStateAdapter(fragmentActivity) {
+    private val itemCount = 2
+
+    override fun getItemCount(): Int {
+        return itemCount
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return UsersFragment(position)
+    }
 }
