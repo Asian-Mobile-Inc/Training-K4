5d2fa86fc7918da5d2131842ddb43e7a1c7ff8c4
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-08-27 11:12:54 +0700

    create ViewHolder

--- app/src/am_huynn/com/example/asian/adapter/PicturesAdapter.kt
+++ app/src/am_huynn/com/example/asian/adapter/PicturesAdapter.kt
@@ -1,3 +1,33 @@
 package com.example.asian.adapter
 
-class PicturesAdapter {}
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.asian.databinding.ItemGridImageBinding
+import com.example.asian.model.Picture
+
+class PicturesAdapter(private val context: Context) :
+    RecyclerView.Adapter<PicturesAdapter.PictureViewHolder>() {
+    private val pictures: MutableList<Picture> = mutableListOf()
+
+    inner class PictureViewHolder(private val binding: ItemGridImageBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bin(picture: Picture) {
+            Glide.with(context).load(picture.uri).into(binding.ivPicture)
+            binding.tvNamePicture.text = picture.name
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PictureViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return pictures.size
+    }
+
+    override fun onBindViewHolder(holder: PictureViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
