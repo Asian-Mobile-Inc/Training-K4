61e4219e2ae6180cc6166fcc7ecd42b747065bb5
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-08-27 09:06:45 +0700

    create UI StorageActivity

---
+++ app/src/am_huynn/com/example/asian/ui/StorageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.asian.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.asian.R
+
+class StorageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_storage)
+    }
+}
