6bbd4f1992de146e527acaa62204eb128a77802f
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-08-28 09:28:54 +0700

    update midel Picture with Entity

--- app/src/am_huynn/com/example/asian/model/Picture.kt
+++ app/src/am_huynn/com/example/asian/model/Picture.kt
@@ -1,5 +1,13 @@
 package com.example.asian.model
 
 import android.net.Uri
+import androidx.room.ColumnInfo
+import androidx.room.Entity
 
-data class Picture(val id: Long, val name: String, val uri: Uri)
+@Entity(tableName = "pictures")
+data class Picture(
+    @ColumnInfo(name = "column_id") val id: Long,
+    @ColumnInfo(name = "column_name") val name: String,
+    @ColumnInfo(name = "column_uri") val uri: Uri,
+    @ColumnInfo(name = "column_favorite") val favorite: Boolean = false
+)
