83d88ff1458f7756cb1b11b41980b2a1561f9529
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-09-04 10:30:07 +0700

    create binding

--- app/src/am_huynn/com/example/asian/ui/ImagesActivity.kt
+++ app/src/am_huynn/com/example/asian/ui/ImagesActivity.kt
@@ -1,35 +1,32 @@
 package com.example.asian.ui
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.example.asian.R
-import com.example.asian.model.Picture
-import com.example.asian.services.remote.ApiClient
-import com.example.asian.services.remote.ApiService
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.example.asian.databinding.ActivityImagesBinding
 
 class ImagesActivity : AppCompatActivity() {
+    private val activityImagesBinding: ActivityImagesBinding by lazy {
+        ActivityImagesBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_images)
+        setContentView(activityImagesBinding.root)
 
-        val apiService = ApiClient.retrofit.create(ApiService::class.java)
-        val call = apiService.getPhotos()
-
-        call.enqueue(object : Callback<List<Picture>> {
-            override fun onResponse(call: Call<List<Picture>>, response: Response<List<Picture>>) {
-                if (response.isSuccessful) {
-                    val pictures = response.body() ?: emptyList()
-                }
-            }
-
-            override fun onFailure(call: Call<List<Picture>>, t: Throwable) {
-                Log.e("TAG", "onFailure: ")
-            }
-
-        })
+//        val apiService = ApiClient.retrofit.create(ApiService::class.java)
+//        val call = apiService.getPhotos()
+//
+//        call.enqueue(object : Callback<List<Picture>> {
+//            override fun onResponse(call: Call<List<Picture>>, response: Response<List<Picture>>) {
+//                if (response.isSuccessful) {
+//                    val pictures = response.body() ?: emptyList()
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<List<Picture>>, t: Throwable) {
+//                Log.e("TAG", "onFailure: ")
+//            }
+//
+//        })
     }
 }
