8183f47392d8c30949272abae890e5171a69877b
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-09-04 10:07:10 +0700

    create DiffCallBack for Picture

---
+++ app/src/am_huynn/com/example/asian/diff/PictureDiffCallBack.kt
@@ -0,0 +1,21 @@
+package com.example.asian.diff
+
+import androidx.recyclerview.widget.DiffUtil
+import com.example.asian.model.Picture
+
+class PictureDiffCallBack(
+    private val oldList: MutableList<Picture>, private val newList: MutableList<Picture>
+) : DiffUtil.Callback() {
+    override fun getOldListSize(): Int = oldList.size
+    override fun getNewListSize(): Int = newList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition].imageId == newList[newItemPosition].imageId
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldUser = oldList[oldItemPosition]
+        val newUser = newList[newItemPosition]
+        return oldUser.permalinkUrl == newUser.permalinkUrl && oldUser.url == newUser.url && oldUser.type == newUser.type && oldUser.thumb_url == newUser.thumb_url && oldUser.created_at == newUser.created_at
+    }
+}
