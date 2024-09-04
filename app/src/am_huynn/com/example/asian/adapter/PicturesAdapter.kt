692edfd4f12d697e8f5866dc086f5f8f365fba40
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-09-04 10:13:55 +0700

    create PicturesAdapter

--- app/src/am_huynn/com/example/asian/adapter/PicturesAdapter.kt
+++ app/src/am_huynn/com/example/asian/adapter/PicturesAdapter.kt
@@ -1,4 +1,50 @@
 package com.example.asian.adapter
 
-class PicturesAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.asian.databinding.ItemGridImageBinding
+import com.example.asian.diff.PictureDiffCallBack
+import com.example.asian.model.Picture
+
+class PicturesAdapter(
+    private val onClick: (Picture) -> Unit
+) : RecyclerView.Adapter<PicturesAdapter.PictureViewHolder>() {
+    private val pictures: MutableList<Picture> = mutableListOf()
+
+    fun setData(newList: MutableList<Picture>) {
+        val diffCallback = PictureDiffCallBack(pictures, newList)
+        val diffCourses = DiffUtil.calculateDiff(diffCallback)
+        pictures.clear()
+        pictures.addAll(newList)
+        diffCourses.dispatchUpdatesTo(this)
+    }
+
+    inner class PictureViewHolder(
+        private val binding: ItemGridImageBinding, private val context: Context
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(picture: Picture) {
+            with(binding) {
+                Glide.with(context).load(picture.url).into(ivPicture)
+                root.setOnClickListener { onClick(picture) }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PictureViewHolder {
+        val binding: ItemGridImageBinding =
+            ItemGridImageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PictureViewHolder(binding, parent.context)
+    }
+
+    override fun getItemCount(): Int {
+        return pictures.size
+    }
+
+    override fun onBindViewHolder(holder: PictureViewHolder, position: Int) {
+        holder.bind(pictures[position])
+    }
 }
