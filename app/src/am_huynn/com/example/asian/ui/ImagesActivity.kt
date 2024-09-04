89f782581fe9155a0ef503558175e01f01756cc4
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-09-04 09:50:30 +0700

    create ImageActivity

--- app/src/am_huynn/com/example/asian/ui/ImagesActivity.kt
+++ app/src/am_huynn/com/example/asian/ui/ImagesActivity.kt
@@ -1,11 +1,35 @@
 package com.example.asian.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.asian.R
+import com.example.asian.model.Picture
+import com.example.asian.retrofit.ApiClient
+import com.example.asian.retrofit.ApiService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ImagesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_images)
+
+        val apiService = ApiClient.retrofit.create(ApiService::class.java)
+        val call = apiService.getPhotos()
+
+        call.enqueue(object : Callback<List<Picture>> {
+            override fun onResponse(call: Call<List<Picture>>, response: Response<List<Picture>>) {
+                if (response.isSuccessful) {
+                    val pictures = response.body() ?: emptyList()
+                }
+            }
+
+            override fun onFailure(call: Call<List<Picture>>, t: Throwable) {
+                Log.e("TAG", "onFailure: ")
+            }
+
+        })
     }
 }
