4ee57713f6d16904eac3d18552016ab67e037510
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-09-04 09:50:16 +0700

    create object ApiClient & interface ApiService

--- app/src/am_huynn/com/example/asian/retrofit/ApiClient.kt
+++ app/src/am_huynn/com/example/asian/retrofit/ApiClient.kt
@@ -1,4 +1,13 @@
 package com.example.asian.retrofit
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object ApiClient {
+    private const val BASE_URL = "https://api.gyazo.com/api/"
+
+    val retrofit: Retrofit by lazy {
+        Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
--- app/src/am_huynn/com/example/asian/retrofit/ApiService.kt
+++ app/src/am_huynn/com/example/asian/retrofit/ApiService.kt
@@ -1,4 +1,12 @@
 package com.example.asian.retrofit
 
-interface ApiServices {
+import com.example.asian.model.Picture
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+
+interface ApiService {
+    @Headers("Authorization: Bearer 5FiuqhBFKuWZa8YG516uTRAyoqPTU4KfMMzoNt4jpMQ")
+    @GET("images")
+    fun getPhotos(): Call<List<Picture>>
 }
