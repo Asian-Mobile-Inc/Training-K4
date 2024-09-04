c30a4e0d788182cbc21f31feb7a41062736a69b2
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-09-04 15:41:19 +0700

    update retrofit two 2 url

--- app/src/am_huynn/com/example/asian/repository/ImageRepository.kt
+++ app/src/am_huynn/com/example/asian/repository/ImageRepository.kt
@@ -1,11 +1,17 @@
 package com.example.asian.repository
 
 import android.app.Application
+import com.example.asian.constants.Constants
 import com.example.asian.services.remote.ApiClient
 import com.example.asian.services.remote.ApiService
+import okhttp3.MultipartBody
 
 class ImageRepository(private val app: Application) {
-    private val apiService = ApiClient.retrofit.create(ApiService::class.java)
+    private val apiService = ApiClient.retrofit(Constants.BASE_URL).create(ApiService::class.java)
+    private val apiServiceUpload =
+        ApiClient.retrofit(Constants.BASE_URL_UPLOAD).create(ApiService::class.java)
 
     fun getImages() = apiService.getPhotos()
+
+    fun uploadImage(image: MultipartBody.Part) = apiServiceUpload.uploadImage(image)
 }
--- app/src/am_huynn/com/example/asian/services/remote/ApiService.kt
+++ app/src/am_huynn/com/example/asian/services/remote/ApiService.kt
@@ -1,12 +1,17 @@
 package com.example.asian.services.remote
 
 import com.example.asian.model.Picture
+import okhttp3.MultipartBody
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Headers
+import retrofit2.http.*
 
 interface ApiService {
     @Headers("Authorization: Bearer 5FiuqhBFKuWZa8YG516uTRAyoqPTU4KfMMzoNt4jpMQ")
     @GET("images")
     fun getPhotos(): Call<MutableList<Picture>>
+
+    @Multipart
+    @Headers("Authorization: Bearer 5FiuqhBFKuWZa8YG516uTRAyoqPTU4KfMMzoNt4jpMQ")
+    @POST("upload")
+    fun uploadImage(@Part image: MultipartBody.Part): Call<Picture>
 }
