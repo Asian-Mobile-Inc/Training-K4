841d45bb5efbd9838dc10c3658fdefe2dff84d32
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-09-04 09:49:39 +0700

    create data class Picture

--- app/src/am_huynn/com/example/asian/model/Picture.kt
+++ app/src/am_huynn/com/example/asian/model/Picture.kt
@@ -1,3 +1,12 @@
 package com.example.asian.model
 
-data class Picture()
+import com.google.gson.annotations.SerializedName
+
+data class Picture(
+    @SerializedName("image_id") val imageId: String,
+    @SerializedName("permalink_url") val permalinkUrl: String,
+    @SerializedName("url") val url: String,
+    @SerializedName("type") val type: String,
+    @SerializedName("thumb_url") val thumb_url: String,
+    @SerializedName("created_at") val created_at: String
+)
