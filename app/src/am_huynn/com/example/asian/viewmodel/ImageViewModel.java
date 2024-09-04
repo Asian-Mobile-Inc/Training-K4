2cb79c32ba1c624d19cab3509c2553c16ffbda44
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-09-04 10:18:25 +0700

    create ImageViewModel

--- app/src/am_huynn/com/example/asian/viewmodel/ImageViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.asian.viewmodel;
-
-import androidx.lifecycle.ViewModel;
-
-public class ImageViewModel extends ViewModel {
-
-}
---
+++ app/src/am_huynn/com/example/asian/viewmodel/ImagesViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.asian.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class ImagesViewModel(private val app: Application) : AndroidViewModel(app) {
+    
+}
