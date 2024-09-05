cdc7838c9394be4f525bc5b1941c48a2262b2ac7
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-09-05 13:57:46 +0700

    change name model

--- app/src/am_huynn/com/example/asian/model/Picture.kt
+++ app/src/am_huynn/com/example/asian/model/Picture.kt
@@ -8,10 +8,10 @@ import com.google.gson.annotations.SerializedName
 @Entity(tableName = "pictures")
 data class Picture(
     @PrimaryKey @ColumnInfo(name = "column_id") @SerializedName("image_id") var imageId: String,
-    @ColumnInfo(name = "column_permalink_url") @SerializedName("permalink_url") var permalinkUrl: String?,
     @ColumnInfo(name = "column_url") @SerializedName("url") var url: String,
+    @ColumnInfo(name = "column_permalink_url") @SerializedName("permalink_url") var permalinkUrl: String?,
     @ColumnInfo(name = "column_type") @SerializedName("type") var type: String?,
-    @ColumnInfo(name = "column_thumb_url") @SerializedName("thumb_url") var thumb_url: String?,
-    @ColumnInfo(name = "column_created_at") @SerializedName("created_at") var created_at: String?,
+    @ColumnInfo(name = "column_thumb_url") @SerializedName("thumb_url") var thumbUrl: String?,
+    @ColumnInfo(name = "column_created_at") @SerializedName("created_at") var createdAt: String?,
     @ColumnInfo(name = "column_favorite") var favorite: Boolean = false
 )
