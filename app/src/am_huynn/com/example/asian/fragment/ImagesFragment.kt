febac3234712be84ea38e59911832e9f27769d79
huynguyenngoc299 <huynguyenngoc299@gmail.com>
2024-09-06 16:23:06 +0700

    create layout dialog show image

--- app/src/am_huynn/com/example/asian/fragment/ImagesFragment.kt
+++ app/src/am_huynn/com/example/asian/fragment/ImagesFragment.kt
@@ -1,17 +1,19 @@
 package com.example.asian.fragment
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.GridLayoutManager
+import com.bumptech.glide.Glide
 import com.example.asian.R
 import com.example.asian.adapter.GridSpacingItemDecoration
 import com.example.asian.adapter.PicturesAdapter
+import com.example.asian.databinding.DialogShowImageBinding
 import com.example.asian.databinding.FragmentImagesBinding
 import com.example.asian.model.Picture
 import com.example.asian.viewmodel.ImagesViewModel
@@ -72,7 +74,14 @@ class ImagesFragment(private val position: Int, onDownLoad: (Picture) -> Unit) :
         }
     }
 
-    private val onItemClick: (Picture) -> Unit = {}
+    private val onItemClick: (Picture) -> Unit = {
+        val dialogBinding = DialogShowImageBinding.inflate(LayoutInflater.from(context))
+        AlertDialog.Builder(context).apply {
+            setView(dialogBinding.root)
+            Glide.with(context).load(it.url).into(dialogBinding.ivPictureDialog)
+            dialogBinding.btnDelete.setOnClickListener {}
+        }.show()
+    }
 
     private val onFavorite: (Picture) -> Unit = {
         when (position) {
